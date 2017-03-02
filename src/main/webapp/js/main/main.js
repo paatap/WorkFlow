4504616eea68b3fb8436ad2363e4366ff7bf9710
paatap <paatap@gmail.com>
2017-03-02 15:12:39 +0400

    h

--- src/main/webapp/js/main/main.js
+++ src/main/webapp/js/main/main.js
@@ -1,3 +1,23 @@
+function printParams(){
+		var qs = (function (a) {
+            if (a == "")
+                return {};
+            var b = {};
+            for (var i = 0; i < a.length; ++i)
+            {
+                var p = a[i].split('=', 2);
+                if (p.length == 1)
+                    b[p[0]] = "";
+                else
+                    b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+            }
+            return b;
+        })(window.location.search.substr(1).split('&'));
+		alert("Anum="+qs.anum+" bnum="+qs.bnum);
+		
+	}
+printParams();
+
 isc.HLayout.create({
     width: "100%",
     height: "100%",
